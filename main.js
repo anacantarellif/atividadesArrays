669695734407770780645fc58da0629fb43aee53
cantarelliana <accffers@gmail.com>
2023-04-28 20:58:14 -0300

    exercícios

---
+++ main.js
@@ -0,0 +1,25 @@
+// const cachorros = ["caramelo", "schnauzer", "golden", "husky", "rottweiler"];
+// numero=number(prompt("digite um número de 0 a 4"))
+// console.log(cachorros[numero]);
+
+// cachorros[numero] === undefined ?
+// console.log("Você deve informar un número de 0 a 4") :
+// console.log(cachorros[numero]);
+
+// const cachorros = ["caramelo", "schnauzer", "golden", "husky", "rottweiler"];
+// cachorros.splice(1,1)
+// console.log(cachorros);
+
+//cachorros.pop()
+// console.log(cachorros.length);
+// console.log(cachorros.includes("caramelo"));
+// novoCachorro = prompt("escreva a raça de outro cachorro")
+// cachorros.push(novoCachorro)
+// console.log(cachorros)
+
+const numeros = [1, 2, 3, 4, 5, 6,]
+console.log(numeros.length);
+numeros.push(7)
+numeros.splice(3,2)
+console.log(numeros.length);
+console.log(numeros);
